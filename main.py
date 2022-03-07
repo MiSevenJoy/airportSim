dd777712956688664d016f64675f0a5bdf5a25b7
MiSevenJoy <1250014907@qq.com>
2022-03-07 22:03:49 +0800

    registerd to gym

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sys
-
-from Environment import env_base
+import gym
 
 if __name__ == '__main__':
-    Baoan_Air = env_base.Env_Base()
+    Baoan_Air = gym.make('AirportWorld-v0')
     Baoan_Air.reset()
     while True:
         Baoan_Air.step()
